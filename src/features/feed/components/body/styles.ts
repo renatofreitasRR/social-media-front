bc67d77da0f3cb214f153594b74d6daa1f77b352
renatofreitasRR <natogfreitas@gmail.com>
2022-08-03 20:53:06 -0300

    style: add responsivity in feed

--- src/features/feed/components/body/styles.ts
+++ src/features/feed/components/body/styles.ts
@@ -9,6 +9,11 @@ export const Wrapper = styled.div`
     grid-template-columns: 256px 1fr;
     gap: 2rem;
     align-items: flex-start;
+
+    @media (max-width: 768px) {
+        font-size: 87.5%;
+        grid-template-columns: 1fr;
+    }
 `;
 
 
